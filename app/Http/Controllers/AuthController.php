52365a297a558a85a4c80977291771a56bacd420
Leonardo Herrera Ornelas <lherrera@grupoplexon.com>
2019-03-23 14:57:18 -0600

    login and logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Company;
 use App\File;
+use App\Http\Requests\Login;
 use App\Http\Requests\SignUpClient;
 use App\Http\Requests\SignUpCompany;
 use App\User;
@@ -15,6 +16,42 @@ use Illuminate\Support\Facades\Storage;
 
 class AuthController extends Controller
 {
+    public function login(Login $request)
+    {
+        $credentials = request(['email', 'password']);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Unauthorized',
+            ], 400);
+        }
+
+        $user = $request->user();
+        $tokenResult = $user->createToken('Personal Access Token');
+        $token = $tokenResult->token;
+
+        if ($request->remember_me) {
+            $token->expires_at = now()->addWeeks(1);
+        }
+
+        $token->save();
+
+        return response()->json([
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse($token->expires_at)->toDateTimeString(),
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+
+        return response()->json([
+            'message' => 'Succesfully logged out',
+        ]);
+    }
+
     public function signup_client(SignUpClient $request)
     {
         $user = User::create($request->all());
@@ -41,6 +78,11 @@ class AuthController extends Controller
             'message' => 'Company succesfully created',
             'company' => $company,
         ]);
+    }
+
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
     }
 
     private function uploadProfilePhoto($model, $image)
--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::group(['prefix' => 'auth'], function () {
     Route::post('login', 'AuthController@login');
     Route::post('signup_client', 'AuthController@signup_client');
